353880702cecb0143bcaa1df2b8409cd597e8f72
crisshigh <criss.bougah@gmail.com>
2022-11-18 16:30:41 +0000

    18/11/2022

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 // import img_bk from './images/bk_design.jpeg'
-import img_profil from './images/black.jpg'
 
-function Design(props) {
+function Header() {
+  return (
+    <header>
+      <h1>My strange recipe</h1>
+    </header>
+  )
+}
+
+function Main() {
   return (
     <main>
-      <form>
-        <div class="container">
-          <img src={img_profil} height={150} alt="profil"/><br/>
-          <h3>{props.accueil} {props.name}</h3>
-          <input type="email" id="email" placeholder="Email" required  pattern=".+@convertmoney.com"
-          title="Merci de fournir uniquement une adresse @Convertmoney">
-          </input><br/>
-          
-          <input type="password" id="password" placeholder="Password" required></input>
-          {/*eslint-disable-next-line*/} 
-          <p>Don't have an account? <a href="#">Sign Up</a> Here!</p>
-          <button type="submit">Sign In</button>
-        </div>
-      </form>
+      <p>My recipe list</p>
     </main>
   )
 }
+
+function Footer() {
+  return(
+    <footer>
+      <p>Copyright &copy;2022</p>
+    </footer>
+  )
+}
+
 function App() {
   return (
-    <div className='App'>
-      <Design accueil="Welcome" name="Singa Nada"/>
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   )
 }
