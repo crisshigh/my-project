4ad3820e73b9d80c15a1644acc3fbcab0f05cc19
crisshigh <criss.bougah@gmail.com>
2022-11-20 23:56:43 +0000

    20/11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-// import img_pizza from "./images/recipe_images_pizza.jpg"
-function HiddenOne() {
+
+const listRecettes=[
+  "Premiere recette",
+  "Deuxieme recette",
+  "Troisieme rectte",
+  "Quatrieme recette"
+]
+
+const listRecetteId= listRecettes.map((recette, recette_id)=>(
+  {
+  id:recette_id,
+  nom_recette:recette
+}
+));
+function Header(props) {
   return (
-    <h1>secret</h1>
+    <header>
+      <h1>{props.titre}</h1>
+    </header>
   )
 }
 
-function Normal() {
+function Main(props) {
   return (
-    <h1>Normal</h1>
+    <main>
+      <p>La list de mes {props.mot}<br/>
+      {props.listRecettes.map((recette)=>(<li key={recette.id}>{recette.nom_recette}</li>))}
+      </p>
+    </main>
   )
 }
 
-function App(props) {
-  return(
-    <>
-    {props.cond}?<HiddenOne/>:<Normal/>
-    </>
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright &copy;{props.annee}</p>
+    </footer>
+  )
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <Header titre="Mes recettes etranges"/>
+      <Main mot="distinguees recettes" listRecettes={listRecetteId}/>
+      <Footer annee={new Date().getFullYear()}/>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App cond={false}/>
+    <App/>
 )
 
 // // If you want to start measuring performance in your app, pass a function
